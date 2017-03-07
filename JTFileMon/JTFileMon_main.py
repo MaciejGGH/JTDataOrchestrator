c8afdc2b9ccf345c796b1d48ee2d04f7b1a25cc4
Maciej Grabowski <maciejg@gmail.com>
2017-03-07 13:36:11 +0100

    Initial commit.

---
+++ JTFileMon/JTFileMon_main.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*
+#-------------------------------------------------------------------------------
+# Name:        Job-Triggering File Watcher
+# Purpose:
+#
+# Author:      Maciej Grabowski
+#
+# Created:     22-02-2017
+# Copyright:   (c) Maciej Grabowski 2017
+# Licence:
+#-------------------------------------------------------------------------------
+
+import requests
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler, FileSystemEventHandler
+import ConfigParser
+from os.path import join, dirname
+
+dir = dirname(__file__)
+configFileName = join(dir, r'cfg\filewatcher.cfg')
+try:
+    configReady = False
+    #config = ConfigParser.SafeConfigParser()
+    #Forcing parser to reuse existing letter case
+    config = ConfigParser.RawConfigParser()
+    config.optionxform = lambda option: option
+    config.read(configFileName)
+    sourceDir = config.get('Common', 'SourceDir')
+    schedulerServiceAddress = config.get('Common', 'SchedulerWebAddress')
+    configReady = True
+
+except Exception as e:
+    print e
+#todo: check if path exists
+print 'Monitorig started for path: {0}'.format(sourceDir)
+print 'Ctrl+C to stop.'
+
+class MyEventHandler(FileSystemEventHandler):
+    def __init__(self, observer):
+        self.observer = observer
+
+    def on_any_event(self, event):
+        s = requests.Session()
+        #print "e=", event
+        #print event.__dict__
+        try:
+            affectedFile = event.dest_path.encode('utf-8')
+            destPath = event.dest_path
+            print '{1} {0}'.format(event.event_type, affectedFile)
+        except:
+            if not event.is_directory:
+                affectedFile = event.src_path.encode('utf-8')
+                print '{1} {0}'.format(event.event_type, affectedFile)
+        r = s.put(schedulerServiceAddress, params={'fileName': affectedFile})
+        #print r.status_code, r.text
+
+def main():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    ##path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    #event_handler = LoggingEventHandler()
+    observer = Observer()
+    event_handler = MyEventHandler(observer)
+    observer.schedule(event_handler, sourceDir, recursive=True)
+    observer.start()
+    i=0
+    try:
+        while True:
+            i+=1
+            time.sleep(1)
+            if i == 120:
+                print 'Monitoring {0}...'.format(sourceDir)
+                i=0
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+if __name__ == '__main__':
+    main()
---
+++ JTScheduler/JTScheduler_main.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*
+#-------------------------------------------------------------------------------
+# Name:        Informatica Scheduler
+# Purpose:
+#
+# Author:      Maciej Grabowski
+#
+# Created:     22.02.2017
+# Copyright:   (c) macie 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import cherrypy
+from jinja2 import Environment, FileSystemLoader
+from os.path import join, dirname
+
+affectedFiles=[]
+@cherrypy.expose
+class InformaticaTriggerWebService(object):
+
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self):
+        global affectedFiles
+        template_dir = join(dirname(__file__), 'templates')
+        jinja_env = Environment(loader = FileSystemLoader(template_dir), autoescape = True)
+        template = jinja_env.get_template('JTSchedulerIndex.html')		
+        #return cherrypy.session['mystring']
+        #return ('{0}\n'.format(f.encode('utf-8')) for f in affectedFiles)
+        return template.render(affectedFiles = affectedFiles)
+
+    def POST(self):
+        global affectedFiles
+        ##print '='*150
+        #return affectedFiles
+        li = ''.join('<li>{0}</li>'.format(f) for f in affectedFiles)
+        return '<ul>{0}</ul>'.format(li)
+
+    def PUT(self, fileName):
+        global affectedFiles
+        print fileName.encode('utf-8')
+        #cherrypy.session['mystring'] = [fileName]
+        affectedFiles += [fileName]
+
+    def DELETE(self):
+        cherrypy.session.pop('mystring', None)
+
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+            'tools.response_headers.on': True,
+            ##'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        }
+    }
+    cherrypy.quickstart(InformaticaTriggerWebService(), '/', conf)
