8310386ea84c8c4f6823a71f5868f2bf75c647a7
Maciej Grabowski <maciejg@gmail.com>
2017-03-13 17:28:42 +0100

    Add dummy job/filmatcher setup.

--- JTScheduler/JTScheduler_main.py
+++ JTScheduler/JTScheduler_main.py
@@ -13,33 +13,76 @@
 import cherrypy
 from jinja2 import Environment, FileSystemLoader
 from os.path import join, dirname
+import ConfigParser
 
-affectedFiles=[]
+dir = dirname(__file__)
+configFileName = join(dir, r'cfg\scheduler.cfg')
+
+executors={}
+affectedFiles=[] #list of files coming from FileMatchers
+
+# get config
+try:
+    configReady = False
+    #config = ConfigParser.SafeConfigParser()
+    #Forcing parser to reuse existing letter case
+    config = ConfigParser.RawConfigParser()
+    config.optionxform = lambda option: option
+    config.read(configFileName)
+    ExecutorList = config.get('Common', 'ExecutorList').split(',')
+    for executorName in ExecutorList:
+        executorAddress = config.get('Executors', executorName)
+        executors[executorName] = executorAddress
+
+    configReady = True
+
+except Exception as e:
+    print e
+
+# fetch filematchers
+try:
+    fm = open(join(dir, r'job_config\filematchers.csv'), 'r').readlines()
+    for line in fm:
+        filematcherId, jobId, type, value = line.split(',')
+except Exception as e:
+    print e
+
+
 @cherrypy.expose
-class InformaticaTriggerWebService(object):
-
+class JTSchedulerWebService(object):
     @cherrypy.tools.accept(media='text/plain')
+
+    def listRegistrations(self):
+        print 1
+        #return True
+    listRegistrations.exposed = True
+
     def GET(self):
         global affectedFiles
         template_dir = join(dirname(__file__), 'templates')
-        jinja_env = Environment(loader = FileSystemLoader(template_dir), autoescape = True)
-        template = jinja_env.get_template('JTSchedulerIndex.html')		
-        #return cherrypy.session['mystring']
-        #return ('{0}\n'.format(f.encode('utf-8')) for f in affectedFiles)
+        jinja_env = Environment(loader=FileSystemLoader(template_dir), autoescape=True)
+        template = jinja_env.get_template('JTSchedulerIndex.html')
+        # return cherrypy.session['mystring']
+        # return ('{0}\n'.format(f.encode('utf-8')) for f in affectedFiles)
         return template.render(affectedFiles = affectedFiles)
 
     def POST(self):
         global affectedFiles
-        ##print '='*150
-        #return affectedFiles
+        # print '='*150
+        # return affectedFiles
         li = ''.join('<li>{0}</li>'.format(f) for f in affectedFiles)
         return '<ul>{0}</ul>'.format(li)
 
     def PUT(self, fileName):
+        def checkJobsToActivate(fileName):
+            pass
         global affectedFiles
         print fileName.encode('utf-8')
-        #cherrypy.session['mystring'] = [fileName]
+        # cherrypy.session['mystring'] = [fileName]
         affectedFiles += [fileName]
+        # check filematchers for jobs to be activated
+        # send notification to Executor if FM found
+
 
     def DELETE(self):
         cherrypy.session.pop('mystring', None)
@@ -54,4 +97,4 @@ if __name__ == '__main__':
             ##'tools.response_headers.headers': [('Content-Type', 'text/plain')],
         }
     }
-    cherrypy.quickstart(InformaticaTriggerWebService(), '/', conf)
+    cherrypy.quickstart(JTSchedulerWebService(), '/', conf)
