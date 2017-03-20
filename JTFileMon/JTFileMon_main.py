8f1a156c6e8836ba16cc3c1d18807342124a5e77
Maciej Grabowski <maciejg@gmail.com>
2017-03-20 17:11:06 +0100

    Move declarations to __init__, add job and filewatcher classes and read on init, rename configs to *.cfg.

--- JTFileMon/JTFileMon_main.py
+++ JTFileMon/JTFileMon_main.py
@@ -19,28 +19,27 @@ from watchdog.events import LoggingEventHandler, FileSystemEventHandler
 import ConfigParser
 from os.path import join, dirname
 
-dir = dirname(__file__)
-configFileName = join(dir, r'cfg\filewatcher.cfg')
-try:
-    configReady = False
-    #config = ConfigParser.SafeConfigParser()
-    #Forcing parser to reuse existing letter case
-    config = ConfigParser.RawConfigParser()
-    config.optionxform = lambda option: option
-    config.read(configFileName)
-    sourceDir = config.get('Common', 'SourceDir')
-    schedulerServiceAddress = config.get('Common', 'SchedulerWebAddress')
-    configReady = True
-
-except Exception as e:
-    print e
-#todo: check if path exists
-print 'Monitorig started for path: {0}'.format(sourceDir)
-print 'Ctrl+C to stop.'
-
 class MyEventHandler(FileSystemEventHandler):
     def __init__(self, observer):
         self.observer = observer
+        dir = dirname(__file__)
+        configFileName = join(dir, r'cfg\filewatcher.cfg')
+        try:
+            configReady = False
+            #config = ConfigParser.SafeConfigParser()
+            #Forcing parser to reuse existing letter case
+            config = ConfigParser.RawConfigParser()
+            config.optionxform = lambda option: option
+            config.read(configFileName)
+            self.sourceDir = config.get('Common', 'SourceDir')
+            self.schedulerServiceAddress = config.get('Common', 'SchedulerWebAddress')
+            configReady = True
+
+        except Exception as e:
+            print e
+        #todo: check if path exists
+        print 'Monitorig started for path: {0}'.format(self.sourceDir)
+        print 'Ctrl+C to stop.'
 
     def on_any_event(self, event):
         s = requests.Session()
@@ -54,7 +53,7 @@ class MyEventHandler(FileSystemEventHandler):
             if not event.is_directory:
                 affectedFile = event.src_path.encode('utf-8')
                 print '{1} {0}'.format(event.event_type, affectedFile)
-        r = s.put(schedulerServiceAddress, params={'fileName': affectedFile})
+        r = s.put(self.schedulerServiceAddress, params={'fileName': affectedFile})
         print r.status_code, r.text
 
 def main():
@@ -65,7 +64,7 @@ def main():
     #event_handler = LoggingEventHandler()
     observer = Observer()
     event_handler = MyEventHandler(observer)
-    observer.schedule(event_handler, sourceDir, recursive=True)
+    observer.schedule(event_handler, event_handler.sourceDir, recursive=True)
     observer.start()
     i=0
     try:
--- JTScheduler/JTScheduler_main.py
+++ JTScheduler/JTScheduler_main.py
@@ -17,19 +17,49 @@ from os import getcwd
 import ConfigParser
 
 
+class job():
+    def __init__(self, line):
+        print line
+        print len(line)
+        jobId, executorName, name, description, groupId, statusId, type, expectedBy, maxRunTime, maxRetryCnt, maxThreads, updatedBy, updatedOn, version = line
+        self.jobId=jobId.strip()
+        self.executorName=executorName.strip()
+        self.name=name.strip()
+        self.description=description
+        self.groupId=groupId.strip()
+        self.statusId=statusId.strip()
+        self.type=type.strip()
+        self.expectedBy=expectedBy.strip()
+        self.maxRunTime=maxRunTime.strip()
+        self.maxRetryCnt=maxRetryCnt.strip()
+        self.maxThreads=maxThreads.strip()
+        self.updatedBy=updatedBy.strip()
+        self.updatedOn=updatedOn.strip()
+        self.version=version.strip()
 
+class filematcher():
+    def __init__(self, line):
+        filematcherId, jobId, type, value = line
+        self.filematcherId=filematcherId
+        self.jobId=jobId
+        self.type=type
+        self.value=value
 
-
-##@cherrypy.expose
 class JTSchedulerWebService(object):
     @cherrypy.tools.accept(media='text/plain')
 
     def __init__(self):
         self.executors={}
         self.affectedFiles=[] #list of files coming from FileMatchers
+        self.jobs={}
+        self.filematchers={}
         # get config
         dir = dirname(__file__)
         configFileName = join(dir, r'cfg\scheduler.cfg')
+        for jobLine in  open(join(dir, r'job_config\jobs.cfg'), 'r').readlines()[1:]:
+            self.jobs[jobLine.split(',')[0]] = job(jobLine.replace('\n', '').split(','))
+        for jobLine in open(join(dir, r'job_config\filematchers.cfg'), 'r').readlines()[1:]:
+            self.filematchers[jobLine.split(',')[0]] = filematcher(jobLine.replace('\n', '').split(','))
 
         # fetch filematchers
         try:
@@ -57,11 +87,9 @@ class JTSchedulerWebService(object):
             print e
 
     def listRegistrations(self):
-        print 1
-        return 'HW'
+        return '<ul>' + ''.join('<li>{0}</li>'.format(f) for f in self.affectedFiles) + '</ul>'
     listRegistrations.exposed = True
 
-##    ##def GET(self):
     def status(self):
         template_dir = join(dirname(__file__), 'templates')
         jinja_env = Environment(loader=FileSystemLoader(template_dir), autoescape=True)
@@ -80,13 +108,12 @@ class JTSchedulerWebService(object):
 
     def registerFile(self):
 ##        #def PUT(self, fileName):
-##        def checkJobsToActivate(fileName):
-##            pass
-##        print '<>'*150
-##        print cherrypy.request.params##['fileName']
-##        #self.affectedFiles += [fileName]
-##        # check filematchers for jobs to be activated
-##        # send notification to Executor if FM found
+        def checkJobsToActivate(fileName):
+            pass
+        print cherrypy.request.params##['fileName']
+        self.affectedFiles += [fileName]
+        # check filematchers for jobs to be activated
+        # send notification to Executor if FM found
         return 'fileName'
     registerFile.exposed = True
 
@@ -105,7 +132,7 @@ if __name__ == '__main__':
     config = {
         'global': {
 ##            'environment': 'production',
-##            'log.screen': True,
+            'log.screen': True,
             'server.socket_host': '127.0.0.1',
             'server.socket_port': 8080,
             'engine.autoreload_on': True,
@@ -113,19 +140,19 @@ if __name__ == '__main__':
             'log.access_file': join(current_dir, 'access.log'),
         },
 
-##            '/': {
-##                #'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-##                'tools.sessions.on': True,
-##                #'tools.staticdir.root': current_dir
-##        },
-##            '/registerFile': {
-##                #'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-##                'tools.sessions.on': True,
-##                ##'tools.staticdir.root': current_dir
-##        },
+            '/registerFile': {
+                #'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+                'tools.sessions.on': True,
+        },
             '/css': {
                 'tools.staticdir.on': True,
                 'tools.staticdir.dir': '/css'
+        },
+            '/job_config': {
+                'tools.staticfile.debug': True,
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': '/job_config'
+
         },
             '/css/mystyle.css': {
                 'tools.staticfile.debug': True,
